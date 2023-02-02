662d5526d4f9cb3e92a03f744ac49d55f55ed85d
Victor <victorfaccioli@hotmail.com>
2023-02-02 00:37:46 -0300

    Feito a validação do cep em a biblioteca do viacep onde ferificar se o cep esta errado ou correto. Caso errado aparece alert de erro, caso estiver correto o mesmo preenche o logradora, bairro, cidade e estado

--- assets/JS/script-WaitingList.js
+++ assets/JS/script-WaitingList.js
@@ -1,33 +1,48 @@
-// Validação senha
-const senha = document.getElementById("senha")
-const confirmarSenha = document.getElementById("confirmarSenha");
-  
- function validarSenha(){
-     if (senha.value !== confirmarSenha.value) {
-         confirmarSenha.setCustomValidity("Senhas diferentes!");
-     }
-     else {
-         confirmarSenha.setCustomValidity('');
-     }
-}
- 
-// Validação e-mail
+  $(document).ready(function() {
 
-function validarEmail(){
-  var email = document.querySelector('#email');
-  var error = document.querySelector('#error');
-  
-  if(!email.checkValidity()){
-    error.innerHTML = "Email invalido";  
-  }
-   
-}
+    function limpa_formulário_cep() {
+        $("#st").val("");
+        $("#inputBairro").val("");
+        $("#inputCidade").val("");
+        $("#inputEstado").val("");
+    }
+    
+    $("#inputCEP").blur(function() {
 
-function redefinirMsg(){
-  var error = document.querySelector('#error');
-  if (error.innerHTML == "Email invalido"){
-    error.innerHTML = "";
-  }
-}
+        var cep = $(this).val().replace(/\D/g, '');
 
+        if (cep != "") {
 
+            var validacep = /^[0-9]{8}$/;
+
+            if(validacep.test(cep)) {
+
+                $("#st").val("...");
+                $("#inputBairro").val("...");
+                $("#inputCidade").val("...");
+                $("#inputEstado").val("...");
+
+                $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
+
+                    if (!("erro" in dados)) {
+                        $("#st").val(dados.logradouro);
+                        $("#inputBairro").val(dados.bairro);
+                        $("#inputCidade").val(dados.localidade);
+                        $("#inputEstado").val(dados.uf);
+                    }
+                    else {
+                        limpa_formulário_cep();
+                        alert("CEP não encontrado.");
+                    }
+                });
+            }
+            else {
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } 
+        else {
+            limpa_formulário_cep();
+        }
+    });
+});
